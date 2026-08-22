ef39517acdc93ebe33b7957c6909c152d64fee8c
Eric Jastrzębski <eric.jastrzebski3@gmail.com>
2026-08-22 14:51:33 +0200

    Setting a project to work on
    Making project in SFML

---
+++ Defend Base.cpp
@@ -0,0 +1,23 @@
+﻿#include <SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode({ 800,600 }), "Defend Base");
+
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+
+		window.clear(sf::Color::Black);
+
+		window.display();
+	}
+
+
+	return 0;
+}
